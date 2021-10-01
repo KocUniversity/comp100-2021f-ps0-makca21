40e4c60ccae674d2979e375ca453d939221c1fe3
MISLINA AKCA <91735737+makca21@users.noreply.github.com>
2021-10-01 13:52:31 +0000

    done

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy
+
+x= input('Enter number x : ')
+print(x)
+y= input('Enter number y : ')
+print(y)
+print('x**y=')
+print(int(x)**int(y))
+print('log(x)')
+print(numpy.log2(int(x)))
+print('80006')
